10e2dff625bcbe0be187dd9f1ffa1ef5fd9f19b6
moqaddus-rafi-brainx <moqaddus.rafi@brainxtech.com>
2025-06-26 11:47:45 +0500

    removed extra code

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,19 +1,8 @@
-const OpenAI = require('openai');
 const {
   generateEmailFromOpenAI,
-  resetMessages,
+  resetMessages
 } = require('../services/openaiService');
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-let messages = [
-  {
-    role: 'system',
-    content: 'You are a helpful assistant that writes professional emails.',
-  },
-];
 
 const generateEmail = async (req, res) => {
   const { prompt,type,tone } = req.body;
